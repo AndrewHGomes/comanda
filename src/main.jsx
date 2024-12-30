d4be9d3c5496cea07b8bfd788836ba278a6b8a87
AndrewHGomes <andrewvianigomes@gmail.com>
2024-12-30 11:19:22 -0300

    Tela de login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./styles/global.css";
+
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
---
+++ src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import main from "../styles/Login.module.css";
+
+const Login = () => {
+  const empresa = "D'affari Sistemas de Informação";
+  return (
+    <>
+      <main className={main.content}>
+        <h1>SICOMANDA 3.0</h1>
+        <form>
+          <select name="login" id="login">
+            <option value="" selected disabled>
+              SELECIONE UM USUÁRIO
+            </option>
+          </select>
+          <input type="password" id="senha" placeholder="SENHA" />
+          <button id="enter">ENTRAR</button>
+        </form>
+        <div>
+          <img src="./daffari.jpeg" alt="" />
+          <h3>{empresa} &copy; 2025</h3>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Login;
